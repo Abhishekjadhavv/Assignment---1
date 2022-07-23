d92a1d96ed87b6852459e9c988ecb2345f960e40
Abhishek jadhav <109812422+Abhishekjadhavv@users.noreply.github.com>
2022-07-23 21:29:59 +0530

    Add files via upload

---
+++ lengthofstring.c
@@ -0,0 +1,10 @@
+// WAP to calculate the length of String using printf function.
+
+#include<stdio.h>
+
+int main(){
+    int length;
+    length=printf("Hello Abhishek");
+    printf("\nlength of string is %d",length);    
+    return 0;
+}
